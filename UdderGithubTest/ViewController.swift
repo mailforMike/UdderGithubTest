c604fff9e43584a11481b3b9272cd2ea534415c4
michael <michael@mholzinger.at>
2018-09-01 13:38:26 +0200

    Initial Commit

---
+++ UdderGithubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UdderGithubTest
+//
+//  Created by Michael Holzinger on 01.09.18.
+//  Copyright © 2018 Michael Holzinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
